51073a8ed8882a69ed81a81faf443d1fa7d6f8fa
victorcodigos <victor.macedo.dedeus@gmail.com>
2023-06-19 03:53:19 +0200

    Ultimos updates, todavia me queda un poco p/ hacer

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ startGameButton.addEventListener("click", startGame) // capturar el evento click
 nextQuestionButton.addEventListener("click", displayNextquestion) // hacer que la siguiente pregunta aparezca en la pantalla
 
 let currentQuestion = 0; // criamos una variable auxilar para saber en cual preguntas estamos
+let totalTheAnswerCorrect = 0; // vamos crear una variable para saber cuántas preguntas acertó el user
 
 function startGame() {
     startGameButton.classList.add("hide") // added a class hide para que el button pueda desaparecer
@@ -20,6 +21,11 @@ function startGame() {
 
 function displayNextquestion() { // esta función es para apresentar la proxima question 
     resetAll()
+
+    if (questions.length == currentQuestion) {
+        return finishTheQuiz() // return para dicer que el juego ha terminado
+    }
+
     questionSpan.textContent = questions[currentQuestion].question // aqui vamos coger la primera pregunta 
     questions[currentQuestion].answers.forEach(answer => {  // esa función flecha es para acceder a las respuestas de la prugunta 1
         const newAnswer = document.createElement("button")   // criar un elemento button para cada una das posibles respuestas 
@@ -51,6 +57,7 @@ function selectAnswerCorrect(e) {  // Cuando tenemos un evento del tipo addEvent
     const answerClicked = e.target
     if (answerClicked.dataset.correct) {  // Si el botón en el que hizo clic tiene el dataSet, entonces su respuesta es correcta.
         document.body.classList.add("correct") // si la respuesta esta correcta, que se quede en color verde 
+        totalTheAnswerCorrect++ // aquí contaremos/implementaremos cuántas preguntas acertó el usuario
     } else {
         document.body.classList.add("incorrect") // si la respuesta esta incorrecta, que se quede en color rojo
     }
@@ -69,6 +76,43 @@ function selectAnswerCorrect(e) {  // Cuando tenemos un evento del tipo addEvent
 
 }
 
+function finishTheQuiz() {
+    const totalQuestion = questions.length
+    const totalHits = Math.floor(totalTheAnswerCorrect * 100 / totalQuestion);
+
+    let message = ""
+
+    switch (true) {
+        case (totalHits >= 90):
+            message = "Excelent :D you are very good on it."
+            break
+        case (totalHits >= 70):
+            message = "Very good :) you just missed 3 questions!"
+            break
+        case (totalHits >= 50):
+            message = "Good! You could be better! You hits half of the quiz!"
+            break
+        default:
+            message = "Ops! I think you need to try it again! :("
+    }
+
+    questionsContainer.innerHTML =
+
+        `
+<p class= final-message>
+   You hit ${totalTheAnswerCorrect} of ${totalQuestion} questions!
+   <span> Results: ${message} </span>
+</p>
+
+
+<button onclick = window.location.reload() class= "button">
+   Try it again! 
+</button>
+
+       `
+
+}
+
 /* criamos una variable questions de tipo array 
  propiedad question que es la propia pregunta 
  propiedad answer que es un array con las posibles respuestas
@@ -76,126 +120,115 @@ function selectAnswerCorrect(e) {  // Cuando tenemos un evento del tipo addEvent
 */
 
 
-const questions = [                                                      
+const questions = [
     {
-        question: "If a 360 is one full rotation before shooting, how many rotations would a 1080 be?",  
-        answers: [                                                       
-            { text: "<4>", correct: false },                   
+        question: "If a 360 is one full rotation before shooting, how many rotations would a 1080 be?",
+        answers: [
+            { text: "<4>", correct: false },
             { text: "<3,5>", correct: false },
             { text: "<3>", correct: true },
-            { text: "<5>", correct: false }                       
+            { text: "<5>", correct: false }
         ]
 
     },
-        
+
     {
-        question: "At the start of a standard game of the Monopoly, if you throw a double six, which square would you land on?", 
-        answers: [                                                      
-            { text: "<Electric Company>", correct: true },                    
+        question: "At the start of a standard game of the Monopoly, if you throw a double six, which square would you land on?",
+        answers: [
+            { text: "<Electric Company>", correct: true },
             { text: "<Water Works>", correct: false },
             { text: "<Chance>", correct: false },
-            { text: "<Community Chest>", correct: false }                       
+            { text: "<Community Chest>", correct: false }
         ]
 
     },
 
     {
-        question: "What was the nickname given to the Hughes H-4 Hercules, a heavy transport flying boat which achieved flight in 1947?", 
-        answers: [                                                      
-            { text: "<Fat Man>", correct: false },                    
+        question: "What was the nickname given to the Hughes H-4 Hercules, a heavy transport flying boat which achieved flight in 1947?",
+        answers: [
+            { text: "<Fat Man>", correct: false },
             { text: "<Noah Ark>", correct: false },
             { text: "<Trojan Horse>", correct: false },
-            { text: "<Spruce Goose>", correct: true }                       
+            { text: "<Spruce Goose>", correct: true }
         ]
 
     },
 
+
     {
-        question: "Which of the following is the IATA code for Manchester Airport?", 
-        answers: [                                                      
-            { text: "<EGLL>", correct: false },                    
+        question: "Which of the following is the IATA code for Manchester Airport?",
+        answers: [
+            { text: "<EGLL>", correct: false },
             { text: "<MAN>", correct: true },
             { text: "<LHR>", correct: false },
-            { text: "<EGCC>", correct: false }                       
+            { text: "<EGCC>", correct: false }
         ]
 
     },
 
     {
-        question: "Which musical artist had a prominent role in the 2017 film Kingsman: The Golden Circle?", 
-        answers: [                                                      
-            { text: "<Lady Gaga>", correct: false },                    
+        question: "Which musical artist had a prominent role in the 2017 film Kingsman: The Golden Circle?",
+        answers: [
+            { text: "<Lady Gaga>", correct: false },
             { text: "<Elton John>", correct: true },
             { text: "<Justin Bieber>", correct: false },
-            { text: "<Rihanna>", correct: false }                       
+            { text: "<Rihanna>", correct: false }
         ]
 
     },
 
     {
-        question: "How many episodes were in season five of Samurai Jack?", 
-        answers: [                                                      
-            { text: "<12>", correct: false },                    
+        question: "How many episodes were in season five of Samurai Jack?",
+        answers: [
+            { text: "<12>", correct: false },
             { text: "<11>", correct: false },
             { text: "<10>", correct: true },
-            { text: "<9>", correct: false }                       
+            { text: "<9>", correct: false }
         ]
 
     },
 
     {
-        question: "Who starred as Bruce Wayne and Batman in Tim Burtonv in 1989 movie Batman?", 
-        answers: [                                                      
-            { text: "<Michael Keaton>", correct: true },                    
+        question: "Who starred as Bruce Wayne and Batman in Tim Burtonv in 1989 movie Batman?",
+        answers: [
+            { text: "<Michael Keaton>", correct: true },
             { text: "<George Clooney>", correct: false },
             { text: "<Val Kilmer>", correct: false },
-            { text: "<Adam West>", correct: false }                       
+            { text: "<Adam West>", correct: false }
         ]
 
     },
 
     {
-        question: "Who led the Communist Revolution of Russia?", 
-        answers: [                                                      
-            { text: "<Mikhail Gorbachev>", correct: false },                    
+        question: "Who led the Communist Revolution of Russia?",
+        answers: [
+            { text: "<Mikhail Gorbachev>", correct: false },
             { text: "<Vladimir Putin>", correct: false },
             { text: "<Joseph Stali>", correct: false },
-            { text: "<Vladimir Lenin>", correct: true }                       
+            { text: "<Vladimir Lenin>", correct: true }
         ]
 
     },
 
     {
-        question: "How many teeth does an adult rabbit have?", 
-        answers: [                                                      
-            { text: "<26>", correct: false },                    
+        question: "How many teeth does an adult rabbit have?",
+        answers: [
+            { text: "<26>", correct: false },
             { text: "<35>", correct: false },
             { text: "<28>", correct: true },
-            { text: "<32>", correct: false }                       
+            { text: "<32>", correct: false }
         ]
 
     },
 
     {
-        question: "What is the largest organ of the human body?", 
-        answers: [                                                      
-            { text: "<Skin>", correct: true },                    
+        question: "What is the largest organ of the human body?",
+        answers: [
+            { text: "<Skin>", correct: true },
             { text: "<Heart>", correct: false },
             { text: "<arge Intestine>", correct: false },
-            { text: "<Liver>", correct: false }                       
+            { text: "<Liver>", correct: false }
         ]
 
     },
 ]
-
-
-
-
-
-
-
-
-
-
-
-
